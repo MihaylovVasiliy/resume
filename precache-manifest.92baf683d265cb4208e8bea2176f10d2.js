c3ea00bcf9379c7469c5411b52460d4332f956b6
Vasiliy-home <basen3012@gmail.com>
2020-05-22 00:34:31 +0400

    Updates

---
+++ precache-manifest.92baf683d265cb4208e8bea2176f10d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d1f7690fa417cdee1c452799d3e786",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e8e2754358975192d801",
+    "url": "/resume/static/css/main.a7210c8f.chunk.css"
+  },
+  {
+    "revision": "a7b5cf9be728dd1c4d64",
+    "url": "/resume/static/js/2.5b07c809.chunk.js"
+  },
+  {
+    "revision": "f632643c8749d078ad1d2459523cf1a6",
+    "url": "/resume/static/js/2.5b07c809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e2754358975192d801",
+    "url": "/resume/static/js/main.6638998e.chunk.js"
+  },
+  {
+    "revision": "cf38402d0361881e6dd1",
+    "url": "/resume/static/js/runtime-main.0e5db20d.js"
+  },
+  {
+    "revision": "ed6d65e6a2dac2fd2ce36a1b32a69508",
+    "url": "/resume/static/media/laptop.ed6d65e6.jpg"
+  },
+  {
+    "revision": "c2ff7d4af2f86d1edd605da14fb52f9e",
+    "url": "/resume/static/media/main-picture.c2ff7d4a.jpg"
+  }
+]);
